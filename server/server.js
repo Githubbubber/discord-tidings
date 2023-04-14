6f66a08833bbc480bb47866e9959dca50e189507
Mekesia Brown <mekesiabrown@gmail.com>
2023-04-14 13:55:01 -0400

    refactor: add env var JIK

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const discordRouter = require('./routes/api/discordRouter');
 require('dotenv').config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
