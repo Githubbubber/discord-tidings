19a1a7069b5ceb4f8b5fa484d0d937d6b0e0d815
Mekesia Brown <mekesiabrown@gmail.com>
2023-04-16 03:45:42 -0400

    refactor: add rate limit conditional to route

--- server/server.js
+++ server/server.js
@@ -15,12 +15,30 @@ app.use(cors());
 app.use('/api', discordRouter);
 
 app.get('/auth/discord', (req, res) => {
-    const path_params = req.originalUrl.indexOf("#") !== -1 ? req.originalUrl.split("#")[1] : 
-                        req.originalUrl.indexOf("?") !== -1 ? req.originalUrl.split("?")[1] :
-                        "";
-    const redirect_url = process.env.APP_REDIRECT_URL;
+    const rate_limit = req.headers['X-RateLimit-Limit'];
+    const rate_limit_remaining = req.headers['X-RateLimit-Remaining'];
+    const rate_limit_reset = req.headers['X-RateLimit-Reset'];
+    const rate_limit_reset_after = req.headers['X-RateLimit-Reset-After'];
+    const rate_limit_bucket = req.headers['X-RateLimit-Bucket'];
 
-    res.redirect(redirect_url + path_params);
+    if (!rate_limit_remaining || rate_limit_remaining > 0) {
+        const path_params = req.originalUrl.indexOf("#") !== -1 ? 
+                                req.originalUrl.split("#")[1] : 
+                                req.originalUrl.indexOf("?") !== -1 ? 
+                                    req.originalUrl.split("?")[1] : 
+                                    "";
+        const redirect_url = process.env.APP_REDIRECT_URL;
+
+        res.redirect(redirect_url + path_params);
+    } else {
+        res.status(429).json({
+            error: {
+                code: 429,
+                message: "Too many requests.",
+                retry_after: rate_limit_reset_after,
+            }
+        });
+    }
 });
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
