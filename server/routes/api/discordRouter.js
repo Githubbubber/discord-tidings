749980062ab4da51990425dae9b6614cadabec8c
Mekesia Brown <mekesiabrown@gmail.com>
2023-04-16 03:44:01 -0400

    refactor: add rate limit conditional to all routes

--- server/routes/api/discordRouter.js
+++ server/routes/api/discordRouter.js
@@ -6,13 +6,25 @@ require('dotenv').config();
 discordRouter.get('/', (req, res, next) => {
     const server_info = process.env.DISCORD_SERVER;
     const channel_info = process.env.DISCORD_CHANNEL;
+    const rate_limit_remaining = req.headers['X-RateLimit-Remaining'];
 
-    res.status(200).json({ 
-        discord_get: { 
-            server: server_info,
-            channel: channel_info, 
-        } 
-    });
+    if (!rate_limit_remaining || rate_limit_remaining > 0) {
+        res.status(200).json({ 
+            discord: { 
+                server: server_info,
+                channelId: channel_info, 
+            } 
+        });
+    } else {
+        console.log("Rate limit exceeded from [/]");
+
+        res.status(429).json({
+            error: {
+                code: 429,
+                message: "Too many requests.",
+            }
+        });
+    }
 });
 
 discordRouter.get('/discord/login', (req, res, next) => {
@@ -31,11 +43,24 @@ discordRouter.get('/discord/login', (req, res, next) => {
         `&scope=` +
         `${scope}`;
 
-    res.status(200).json({
-        discord_get_login: {
-            url: get_url,
-        }
-    });
+    const rate_limit_remaining = req.headers['X-RateLimit-Remaining'];
+
+    if (!rate_limit_remaining || rate_limit_remaining > 0) {
+        res.status(200).json({
+            discord_get_login: {
+                url: get_url,
+            }
+        });
+    } else {
+        console.log("Rate limit exceeded from [/discord/login]");
+
+        res.status(429).json({
+            error: {
+                code: 429,
+                message: "Too many requests.",
+            }
+        });
+    }
 });
 
 discordRouter.post('/discord/login', (req, res, next) => {
@@ -48,8 +73,22 @@ discordRouter.post('/discord/login', (req, res, next) => {
         accessToken
     } = user_data;
 
-    res.status(200).json({
-        discord_post_login: { success: true }
-    });
+    const rate_limit_remaining = req.headers['X-RateLimit-Remaining'];
+
+    if (!rate_limit_remaining || rate_limit_remaining > 0) {
+        res.status(200).json({
+            discord_post_login: { success: true }
+        });
+    } else {
+        console.log("Rate limit exceeded from [/discord/login]");
+
+        res.status(429).json({
+            error: {
+                code: 429,
+                message: "Too many requests.",
+            }
+        });
+    }
 });
+
 module.exports = discordRouter;
